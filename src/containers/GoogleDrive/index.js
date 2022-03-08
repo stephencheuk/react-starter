9fcf37cbf70547a0f43f937a9b958d802542c292
stephencheuk <cheuklt@gmail.com>
2022-03-08 23:08:15 +0800

    Update Drive function

--- src/containers/GoogleDrive/index.js
+++ src/containers/GoogleDrive/index.js
@@ -10,6 +10,7 @@ var discoveryUrl = 'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest';
 const GoogleDrive = () => {
   const [user, setUser] = useState(null);
   const [files, setFiles] = useState([]);
+  const [path, setPath] = useState({});
   // const [googleAuth, setGoogleAuth] = useState(null);
 
   const initAuth2 = async () => {
@@ -61,12 +62,16 @@ const GoogleDrive = () => {
     }
   }, [user])
 
+  useEffect(() => {
+    loadFile(path.id);
+  }, [path])
+
   function updateSigninStatus(isSignedIn) {
     // When signin status changes, this function is called.
     // If the signin status is changed to signedIn, we make an API call.
     if (isSignedIn) {
       updateUser(gapi.auth2.getAuthInstance().currentUser.get());
-      loadFile('/root');
+      setPath({ id: 'root', name: '/' });
     }
   }
   // const initClient = async () => {
@@ -88,18 +93,21 @@ const GoogleDrive = () => {
     }
     // this.setState({loading: true})
     var conditions = []
-    conditions.push("trashed = false")
+    //conditions.push("trashed = false")
     //conditions.push("mimeType = 'application/vnd.google-apps.folder'")
     conditions.push("'" + parent + "' in parents")
     var query = encodeURIComponent(conditions.join(" and "))
+    console.log(query);
     // https://www.googleapis.com/drive/v3/files?q=trashed = false and '[object Object]' in parents&fields=files(copyRequiresWriterPermission,createdTime,description,iconLink,id,kind,mimeType,name,ownedByMe,parents,properties,shared,sharingUser,size,teamDriveId,thumbnailLink,trashed,webContentLink,webViewLink),incompleteSearch,kind,nextPageToken&key=
     // https://www.googleapis.com/drive/v3/files?q=trashed = false and 'root' in parents&fields=files(copyRequiresWriterPermission,createdTime,description,iconLink,id,kind,mimeType,name,ownedByMe,parents,properties,shared,sharingUser,size,teamDriveId,thumbnailLink,trashed,webContentLink,webViewLink),incompleteSearch,kind,nextPageToken&key=
     var url = "https://www.googleapis.com/drive/v3/files?q=" + query + "&fields=files(copyRequiresWriterPermission%2CcreatedTime%2Cdescription%2CiconLink%2Cid%2Ckind%2CmimeType%2Cname%2CownedByMe%2Cparents%2Cproperties%2Cshared%2CsharingUser%2Csize%2CteamDriveId%2CthumbnailLink%2Ctrashed%2CwebContentLink%2CwebViewLink)%2CincompleteSearch%2Ckind%2CnextPageToken&key="
+    var url = "https://www.googleapis.com/drive/v3/files?q=" + query + "&fields=files(mimeType, id, name, size, parents)&key="
     //url = 'response.json'
     fetch(url, { method: 'GET', headers, })
       .then(response => response.json())
       .then(data => {
         if (data.files) {
+          console.log(data.files);
           setFiles(data.files);
         }
         // this.setState({ loading: false })
@@ -135,30 +143,48 @@ const GoogleDrive = () => {
   // // <UserCard user={user} />
   if (user) {
     return (
-      <div className="flex">
-        <div className='flex w-[300px]'>
-          <div id="" className="rounded border border-orange-500 bg-orange-700 p-2 cursor-pointer" onClick={e => loadFile('root')}>
-            Load File
+      <div className="flex flex-col">
+        <div className='flex w-full bg-red-500 justify-between'>
+          <div className='flex'>
+            <div id="" className="rounded border border-orange-500 bg-orange-700 p-2 cursor-pointer" onClick={e => loadFile('root')}>
+              Load File
+            </div>
+            <div>
+              <img src={user.profileImg} />
+            </div>
           </div>
-          <div>
-            <img src={user.profileImg} />
-          </div>
-          <div>
-            {user.name}
-          </div>
-          <div id="" className="rounded border border-orange-500 bg-orange-700 p-2 cursor-pointer" onClick={signOut}>
-            Sign Out
+          <div className='flex'>
+            <div className='w-fit'>
+              {user.name}
+            </div>
+            <div id="" className="w-fit rounded border border-orange-500 bg-orange-700 p-2 cursor-pointer" onClick={signOut}>
+              Sign Out
+            </div>
           </div>
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col bg-yellow-500">
+          <div> PATH: {" "}
+            {
+              path.name
+            }
+          </div>
           {
             files.map((file) => {
-              return (
-                <div key={file.id} className='flex' onClick={e => loadFile(file.id)}>
-                  <div className='w-[40px]'><img src={file.iconLink} /></div>
-                  <div className='flex-1'>{file.name}</div>
-                </div>
-              )
+              if (file.mimeType = "application/vnd.google-apps.folder") {
+                return (
+                  <div key={file.id} className='flex' onClick={e => setPath({ 'id': file.id, 'name': file.name })}>
+                    <div className='w-[40px]'><img src={file.iconLink} /></div>
+                    <div className='flex-1'>{file.name}</div>
+                  </div>
+                )
+              } else {
+                return (
+                  <div key={file.id} className='flex' onClick={e => loadFile(file.id)}>
+                    <div className='w-[40px]'><img src={file.iconLink} /></div>
+                    <div className='flex-1'>{file.name}</div>
+                  </div>
+                )
+              }
             })
           }
         </div>
